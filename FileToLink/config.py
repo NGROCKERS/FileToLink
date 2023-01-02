edc02b71c0e774308f68fc47cdec4cbaaebdbec0
NG ROCKERS <77508122+NGROCKERS@users.noreply.github.com>
2023-01-02 21:45:53 +0400

    Update config.py

--- FileToLink/config.py
+++ FileToLink/config.py
@@ -2,17 +2,17 @@ import os
 
 
 class Config:
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    Token = os.environ.get("BOT_TOKEN")
+    API_ID = int(os.environ.get("3563028"))
+    API_HASH = os.environ.get("d119ea76a256e81446faacdb20a204d8")
+    Token = os.environ.get("5061986561:AAFb6oo3zkfvlKqUW1Tu5eOgyu4siKY1SI8")
     Session = os.environ.get("Session_String")
     if Session is None or Session == "":
         Session = ":memory:"
-    App_Name = os.environ.get("APP_NAME")
-    Port = int(os.environ.get("PORT"))
-    Archive_Channel_ID = int(os.environ.get("ARCHIVE_CHANNEL_ID"))
+    App_Name = os.environ.get("mallustream")
+    Port = int(os.environ.get("8080"))
+    Archive_Channel_ID = int(os.environ.get("-1001407831884"))
     Start_Message = os.environ.get("Start_Message")
-    Bot_Channel = os.environ.get("Bot_Channel_UserName")
+    Bot_Channel = os.environ.get("@MALLUROCKERSCLUB")
     if Bot_Channel and Bot_Channel.startswith("@"):
         Bot_Channel = Bot_Channel[1:]
     elif Bot_Channel == "":
